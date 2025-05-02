d3810fa905f6e8df80d246e11ddb33b08621ab28
Artaccann <126908416+Artaccann@users.noreply.github.com>
2025-05-02 10:51:00 +0200

    Update ch1_backgrounds.js

--- Scripts/Chapter_1/ch1_backgrounds.js
+++ Scripts/Chapter_1/ch1_backgrounds.js
@@ -1,12 +1,12 @@
 // Pozadí pro Chapter 1
 const bg_room = new Image();
-bg_room.src = '../Sprites/room.png';
+bg_room.src = '/OSOL/Sprites/room.png';
 
 const bg_oldroom = new Image();
-bg_oldroom.src = '../Sprites/oldroom.png';
+bg_oldroom.src = '/OSOL/Sprites/oldroom.png';
 
 const bg_outside = new Image();
-bg_outside.src = '../Sprites/outside.png';
+bg_outside.src = '/OSOL/Sprites/outside.png';
 
 const CH1_BACKGROUNDS = {
   room: bg_room,
